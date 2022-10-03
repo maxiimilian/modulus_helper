86b4c1621285487eaedb4b1228dfb3ef9de43c54
Maximilian Pierzyna <m.pierzyna@tudelft.nl>
2022-10-03 17:59:25 +0200

    Did not set env variable to modulus version before. Fixed that

--- modulus_jupyter/start_modulus.sh
+++ modulus_jupyter/start_modulus.sh
@@ -6,5 +6,5 @@ mkdir -p data
 docker run --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 \
            --runtime nvidia -v ${PWD}/data:/examples \
            -p $JUPYTER_PORT:8888 \
-           -it --rm modulus:22.09 jupyter lab
+           -it --rm modulus:$MODULUS_VERSION jupyter lab
 

98e899148aff3781721618bfdfe0638b4005b405
Maximilian Pierzyna <m.pierzyna@tudelft.nl>
2022-10-03 17:32:44 +0200

    init with jupyter config

---
+++ modulus_jupyter/start_modulus.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+MODULUS_VERSION=22.09
+JUPYTER_PORT=1234
+
+mkdir -p data
+docker run --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 \
+           --runtime nvidia -v ${PWD}/data:/examples \
+           -p $JUPYTER_PORT:8888 \
+           -it --rm modulus:22.09 jupyter lab
+
